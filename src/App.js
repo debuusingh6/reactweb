24c571528131cf51626561da1310247628c47c2a
debuusingh6 <debuusingh6@gmail.com>
2021-05-23 11:11:19 +0530

    first time

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Newfile from './Newfile';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Newfile />
+      <Newfile />
+    </div>
+  );
+}
+
+export default App;
